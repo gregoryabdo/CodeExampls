38ae99aeff68c1325e65e64a9f6b33f016e399a7
gregoryabdo <gregoryabdo@gregorys-mbp>
2015-02-02 00:03:24 -0800

    First Commit

---
+++ TestProject/src/main/java/testPackage/TestPackage.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class TestPackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!"); // Display the string.
+		
+		
+	}
+
+}
